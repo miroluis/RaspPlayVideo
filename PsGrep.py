e99b65121a682f9db9addd8761a011ba470c0184
MiroLuis <miroluis@gmail.com>
2023-12-16 00:59:06 +0000

    bom

--- PsGrep.py
+++ PsGrep.py
@@ -1,22 +1,47 @@
 #!/usr/bin/python3
 import subprocess
+import re
+import time
+import RPi.GPIO as GPIO
 
+switch = 37
 
-result = subprocess.run(["ps -ef | grep raspplayvideo"], shell=True, capture_output=True, text=True)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print(result.stdout)
+time.sleep(2);
 
-list = result.stdout.splitlines()
+def vlc_toca():
+  result = subprocess.run(["ps -ef | grep RaspPlayVideo/filme.mp4"], shell=True, capture_output=True, text=True)
+  print(result.stdout)
+  list = result.stdout.splitlines()
 
-print(len(list))
+  for element in list:
+    elemento_pedaco = re.split('\s', element)
+    elemento_pedaco = ' '.join(elemento_pedaco).split()
+    print(elemento_pedaco[1])
+    if(elemento_pedaco[6] != "00:00:00"):
+      #print("é para abater")
+      #print(elemento_pedaco[1] + ' -> ' + elemento_pedaco[6])
+      #subprocess.run(["kill " + elemento_pedaco[1]], shell=True, capture_output=True, text=True)
+      #print("já abati")
+      print("retorna true")
+      return True
+  return False
 
-for element in list:
-#for element in range (0, len(list)-2):
-#   print(element.find("raspplayvideo.py"))
+def paraVLC():
+  result = subprocess.run(["ps -ef | grep RaspPlayVideo/filme.mp4"], shell=True, capture_output=True, text=True)
+  list = result.stdout.splitlines()
 
-   if element.find("raspplayvideo.py") > -1 :
-     # print(list[element])
-      print(element)
-
-
-#result.stdout.write(" ".join(list))
+  for element in list:
+    elemento_pedaco = re.split('\s', element)
+    elemento_pedaco = ' '.join(elemento_pedaco).split()
+    if(elemento_pedaco[6] != "00:00:00"):
+      subprocess.run(["kill " + elemento_pedaco[1]], shell=True, capture_output=True, text=True)  
+      
+while (vlc_toca() == True):
+  print("Olha o VLC está a tocar vou esperar pelo botão")
+  time.sleep(0.2);
+  if(GPIO.input(switch) == 0):
+    paraVLC()
+    #subprocess.run(["kill " + elemento_pedaco[1]], shell=True, capture_output=True, text=True)
--- tras_b_raspplayvideo.py
+++ tras_b_raspplayvideo.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 import subprocess
 import RPi.GPIO as GPIO
@@ -5,7 +6,7 @@ import time
 
 #import vlc 
 
-led = 18
+led = 35
 switch = 37
 
 GPIO.setmode(GPIO.BOARD)
@@ -16,22 +17,33 @@ time.sleep(0.2);
 
 print('Switch status = ', GPIO.input(switch))
 
+#while 1:
+#   GPIO.output(led, 1);
+#   time.sleep(1);
+#   GPIO.output(led, 0);
+#   time.sleep(1);
+#GPIO.output(led, 1);
+
+
+
+
 while 1:
     while GPIO.input(switch) == 1:
         time.sleep(0.2);
         print('Switch status = ', GPIO.input(switch))
     else:
         print('Vou tocar o filme')
+        GPIO.output(led, 1);
 
+        subprocess.Popen(["python3", "RaspPlayVideo/PsGrep.py"])
         result = subprocess.run(["vlc RaspPlayVideo/filme.mp4 vlc://quit"], shell=True, capture_output=True, text=True)
 
         print(result.stdout)
 
         print('o filme acabou')
+        time.sleep(1);
+
+        GPIO.output(led, 0);
 
 
 GPIO.cleanup()
-
-
-
-#subprocess.run(["vlc", "bigbuckbunny_30sclip.mp4"]) 
